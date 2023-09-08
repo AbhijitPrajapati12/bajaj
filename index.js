ff2f060621410c1ccc57d88ac991a3e9a9481efb
ABHIJIT PRAJAPATI <SUNNYPRAJAPATI3402@GMAIL.COM>
2023-09-08 16:59:24 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
